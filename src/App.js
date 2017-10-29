21fe565315dec1052fffbb4ebfacedf191aa425b
Dylan Harness <dharness@uwo.ca>
2017-10-28 17:03:31 -0700

    .

--- src/App.js
+++ src/App.js
@@ -1,17 +1,21 @@
 import React, { Component } from 'react';
-import 'react-photoswipe/lib/photoswipe.css';
 import { PhotoSwipe, PhotoSwipeGallery } from 'react-photoswipe'
 import loadImages from './services/loadImages'
+import prompts from './services/prompts'
+import 'react-photoswipe/lib/photoswipe.css';
 import './App.css';
-
-
 
 class App extends Component {
 
   constructor() {
     super()
-    this.state = { items: [] }
+    this.state = {
+      items: [],
+      prompts,
+    }
+    this.date = new Date()
     this.getThumbnailContent = this.getThumbnailContent.bind(this)
+    this.getPrompt = this.getPrompt.bind(this)
   }
 
   componentDidMount() {
@@ -28,9 +32,17 @@ class App extends Component {
     );
   }
 
+  getPrompt() {
+    if(this.date.getMonth() === 9) {
+      return `Prompt: ${this.state.prompts[this.date.getDate()]}`
+    }
+    return `Drawvember hasn't begun`
+  }
+
   render() {
     return (
       <div className="App">
+        <h1 className="prompt">{this.getPrompt()}</h1>
         {
           (this.state.items.length > 0) && 
           <PhotoSwipeGallery items={this.state.items} thumbnailContent={this.getThumbnailContent}/>
